aef3db0789844e8e7be7b3bb735f78a8090bdac5
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2019-05-01 13:41:08 +0700

    variable's name fix in ScorePane

--- src/gui/ScorePane.java
+++ src/gui/ScorePane.java
@@ -2,21 +2,25 @@ package gui;
 
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 public class ScorePane extends VBox{
-	private javafx.scene.control.TextField label;
-	private javafx.scene.control.TextField score;
+	private TextField label;
+	private TextField scoreField;
+	private int score;
 	
-	public ScorePane(int score) {
+	public ScorePane() {
+		
 		this.setPadding(new Insets(10));
+		this.score = 0;
 		this.setPrefHeight(90);
 		this.setPrefWidth(160);
 		this.label.setText("Score");
-		this.score.setText("00000");
-		this.getChildren().addAll(this.label,this.score);
+		this.scoreField.setText(String.format("%05d",score));
+		this.getChildren().addAll(this.label,this.scoreField);
 	}
 	public void update(int score) {
-		this.score.setText(String.format("%05d", score));
+		this.scoreField.setText(String.format("%05d", score));
 	}
 }
