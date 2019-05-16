37e5054a5a55ec0e19378530b49ff343fdc2511e
dorm <sarandorm>
2019-05-16 23:05:10 +0700

    fix nid fix noi

--- src/gui/RetryPane.java
+++ src/gui/RetryPane.java
@@ -12,44 +12,49 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import main.Main;
 
-public class RetryPane extends Menu{
-	private ImageView gameOver = new ImageView(ClassLoader.getSystemResource("GameOver.png").toString());
+public class RetryPane extends Menu {
+	private ImageView gameOver;
 	private static Button restartButton = new Button("Restart");
 	private static Button quitButton = new Button("Quit");
-	private Label yourScore,yourScoreString;
-	
+	private Label yourScore, yourScoreString;
+
 	public RetryPane() {
-		super(restartButton,quitButton);
+		super(restartButton, quitButton);
+		this.yourScoreString = new Label("Your Score");
+		this.yourScore = new Label(String.format("%05d", 0));
+		this.gameOver = new ImageView(ClassLoader.getSystemResource("GameOver.png").toString());
 		this.setPadding(new Insets(10));
 		this.setSpacing(10);
 		this.setAlignment(Pos.CENTER);
 		this.gameOver.setFitHeight(300);
 		this.gameOver.setFitWidth(500);
-		this.yourScoreString = new Label("Your Score");
-		this.yourScore = new Label(String.format("%05d", 0));
-		
-		this.yourScoreString.setFont(Font.loadFont(ClassLoader.getSystemResource("KenneyPixelSquare.ttf").toString(), 32));
+
+		this.yourScoreString
+				.setFont(Font.loadFont(ClassLoader.getSystemResource("KenneyPixelSquare.ttf").toString(), 32));
 		this.yourScore.setFont(Font.loadFont(ClassLoader.getSystemResource("KenneyPixelSquare.ttf").toString(), 30));
 		this.yourScoreString.setTextFill(Color.WHITE);
 		this.yourScore.setTextFill(Color.WHITE);
-		
+
 		restartButton.setFont(Font.loadFont(ClassLoader.getSystemResource("KenneyPixelSquare.ttf").toString(), 30));
 		quitButton.setFont(Font.loadFont(ClassLoader.getSystemResource("KenneyPixelSquare.ttf").toString(), 30));
 		this.setPrefSize(300, 30);
-		this.getChildren().addAll(gameOver,yourScoreString,yourScore,restartButton,quitButton);
+		this.getChildren().addAll(gameOver, yourScoreString, yourScore, restartButton, quitButton);
 		restartButton.setPrefSize(200, 30);
 		quitButton.setPrefSize(200, 30);
 	}
+
 	public ImageView getGameOver() {
 		return gameOver;
 	}
-	
+
 	public void setYourScore(int score) {
 		this.yourScore.setText(String.format("%05d", score));
 	}
+
 	public Button getGameStartButton() {
 		return restartButton;
 	}
+
 	public Button getQuitButton() {
 		return quitButton;
 	}
--- src/gui/ScorePane.java
+++ src/gui/ScorePane.java
@@ -8,32 +8,37 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-public class ScorePane extends VBox{
-	private Label label = new Label();
-	private Label scoreField = new Label();
+
+public class ScorePane extends VBox {
+	private Label scoreLabel;
+	private Label scoreField;
 	private int score;
-	
+
 	public ScorePane() {
-		
+		this.scoreField = new Label();
+		this.scoreLabel = new Label();
 		this.setPadding(new Insets(10));
 		this.setAlignment(Pos.CENTER);
 		this.score = 0;
 		this.setPrefHeight(200);
 		this.setPrefWidth(180);
-		this.label.setText("Score");
-		label.setFont(Font.loadFont(ClassLoader.getSystemResource("KenneyPixelSquare.ttf").toString(), 44));
+		this.scoreLabel.setText("Score");
+		scoreLabel.setFont(Font.loadFont(ClassLoader.getSystemResource("KenneyPixelSquare.ttf").toString(), 44));
 		scoreField.setFont(Font.loadFont(ClassLoader.getSystemResource("KenneyPixelSquare.ttf").toString(), 32));
-		this.scoreField.setText(String.format("%05d",score));
-		this.getChildren().addAll(this.label,this.scoreField);
+		this.scoreField.setText(String.format("%05d", score));
+		this.getChildren().addAll(this.scoreLabel, this.scoreField);
 	}
+
 	public void update(int score) {
 		this.score += score;
 		this.scoreField.setText(String.format("%05d", this.score));
 	}
+
 	public void reset() {
 		this.score = 0;
 		this.scoreField.setText(String.format("%05d", this.score));
 	}
+
 	public int getScore() {
 		return score;
 	}
