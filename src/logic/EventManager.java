26cfe8e6cb7e058868f06309c1f4306420e19219
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2019-05-16 19:13:55 +0700

    main clear

--- src/logic/EventManager.java
+++ src/logic/EventManager.java
@@ -5,7 +5,15 @@ import gui.ControlPane;
 import gui.RetryPane;
 import gui.ScorePane;
 import gui.StartPane;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import main.Main;
 
 public class EventManager {
 
@@ -15,16 +23,25 @@ public class EventManager {
 	private StartPane startPane;
 	private RetryPane retryPane;
 	private SoundManager soundManager;
+	private Stage primaryStage;
+	private Main main;
 
 	public EventManager(ControlPane controlPane, BoardPane boardPane1, BoardPane boardPane2, StartPane startPane,
-			RetryPane retryPane, SoundManager soundManager) {
+			RetryPane retryPane, SoundManager soundManager,Stage primaryStage) {
 
 		this.controlPane = controlPane;
 		this.boardPane1 = boardPane1;
 		this.boardPane2 = boardPane2;
+		this.startPane = startPane;
+		this.retryPane = retryPane;
+		this.primaryStage = primaryStage;
 
+		this.soundManager = soundManager;
+		
 		this.boardManager1 = new BoardManager(boardPane1);
 		this.boardManager2 = new BoardManager(boardPane2);
+		
+		this.main = main;
 	}
 
 	public void setUpArrowButton() {
@@ -45,9 +62,94 @@ public class EventManager {
 		controlPane.getRightButton().addEventHandler(ActionEvent.ACTION,
 				new ArrowButtonEventHandler(Direction.RIGHT, boardManager2));
 	}
+
+	public void setupStartPaneStartButton(Scene scene) {
+		startPane.getGameStartButton().setOnAction(new EventHandler<ActionEvent>() 
+		{
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				soundManager.playMainSong();
+				primaryStage.setScene(scene);
+				boardManager1.generateNewTile();
+				boardManager1.generateNewTile();
+				boardManager2.generateNewTile();
+				boardManager2.generateNewTile();
+			}
+		});
+		
+	}
 	
+	public void setupStartPaneQuitButton() {
+		startPane.getQuitButton().setOnAction(new EventHandler<ActionEvent>() {
 
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+	}
 
+	public void setupRetryPaneStartButton(Scene startScene,VBox dataPane,ScorePane scorePane) {
+		retryPane.getGameStartButton().setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				primaryStage.setScene(startScene);
+				boardManager1.resetBoard();
+				boardManager2.resetBoard();
+				dataPane.getChildren().add(soundManager.getSoundButton());
+				scorePane.reset();
+				ArrowButtonEventHandler.isDead = false;
+				soundManager.stopGameOverSong();
+				soundManager.getSoundButton().setSong("mainSong");
+			}
+		});
+	}
+	
+	public void setupRetryPaneQuitButton() {
+		retryPane.getQuitButton().setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+	}
+	
+	public void setupSceneKeyHandler(Scene scene,ScorePane scorePane,Scene retryScene) {
+		scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+			if (key.getCode() == KeyCode.DOWN) {
+				controlPane.getDownButton().fire();
+			}
+			if (key.getCode() == KeyCode.UP) {
+				controlPane.getUpButton().fire();
+			}
+			if (key.getCode() == KeyCode.LEFT) {
+				controlPane.getLeftButton().fire();
+			}
+			if (key.getCode() == KeyCode.RIGHT) {
+				controlPane.getRightButton().fire();
+			}
+			if (logic.ArrowButtonEventHandler.isDead) {
+				retryPane.setYourScore(scorePane.getScore());
+				primaryStage.setScene(retryScene);
+				soundManager.stopMainSong();
+				retryPane.getChildren().add(soundManager.getSoundButton());
+				if (soundManager.getSoundButton().isSoundOn()) {
+					soundManager.playGameOverSong();
+				}
+				soundManager.getSoundButton().setSong("gameOverSong");
+				
+			}
+		});
+	}
 	public ControlPane getControlPane() {
 		return controlPane;
 	}
--- src/main/Main.java
+++ src/main/Main.java
@@ -38,59 +38,65 @@ import logic.SoundManager;
 import logic.ArrowButtonEventHandler;
 
 public class Main extends Application {
-	
+
 	private ControlPane controlPane;
 	public static ScorePane scorePane;
-	private BoardPane boardPane1;
-	private BoardPane boardPane2;
+	private BoardPane boardPane1, boardPane2;
 	private StartPane startPane;
 	private RetryPane retryPane;
 	private SoundManager soundManager;
-	
+
 	public void start(Stage primaryStage) {
 		HBox main = new HBox();
 		main.setPrefSize(1280, 720);
 		main.setSpacing(10);
 		main.setPadding(new Insets(10));
-		
+
 		Label title = new Label("Elenum");
-		title.setFont(new Font("Blackadder ITC", 72));
-		
+		title.setFont(Font.loadFont(ClassLoader.getSystemResource("KenneyPixelSquare.ttf").toString(), 30));
+
 		main.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
-		
+
 		createGameMaterial();
-		
-		EventManager eventManager = new EventManager(controlPane, boardPane1, boardPane2, startPane, retryPane, soundManager);
+
+		EventManager eventManager = new EventManager(controlPane, boardPane1, boardPane2, startPane, retryPane,
+				soundManager, primaryStage);
 		eventManager.setUpArrowButton();
-		
+
 		BoardManager boardManager1 = eventManager.getBoardManager1();
 		BoardManager boardManager2 = eventManager.getBoardManager2();
 		VBox numPane = new VBox();
 		VBox numPane2 = new VBox();
 		VBox dataPane = new VBox();
 		HBox retry = new HBox();
-		
+
 		retryPane.getChildren().add(soundManager.getSoundButton());
 		controlPane.getChildren().add(soundManager.getSoundButton());
 		retry.getChildren().add(retryPane);
 		retry.setAlignment(Pos.CENTER);
-		retry.setPrefSize(1280, 720);;
+		retry.setPrefSize(1280, 720);
+		;
 		retry.setSpacing(10);
 		retry.setPadding(new Insets(10));
-		
-		numPane.setPrefSize(480, 720);
+
+		numPane.setMinSize(480, 720);
+		numPane.setMaxSize(480, 720);
 		numPane.getChildren().add(boardPane1);
 		numPane.setAlignment(Pos.CENTER);
-		
-		numPane2.setPrefSize(480, 720);
+
+		numPane2.setMinSize(480, 720);
+		numPane2.setMaxSize(480, 720);
 		numPane2.getChildren().add(boardPane2);
 		numPane2.setAlignment(Pos.CENTER);
-		
+
+		dataPane.setMinSize(320, 720);
+		dataPane.setMaxSize(320, 720);
+		dataPane.setPadding(new Insets(20));
 		dataPane.setAlignment(Pos.TOP_CENTER);
-		dataPane.getChildren().addAll(title, scorePane, controlPane,soundManager.getSoundButton());
-		
+		dataPane.getChildren().addAll(title, scorePane, controlPane, soundManager.getSoundButton());
+
 		main.getChildren().addAll(numPane, dataPane, numPane2);
-		
+
 		Scene scene = new Scene(main);
 		HBox start = new HBox();
 		start.setPrefSize(1280, 720);
@@ -104,86 +110,14 @@ public class Main extends Application {
 		primaryStage.setTitle("Elenum");
 		primaryStage.show();
 
-		
-		
-		
-		
-		startPane.getGameStartButton().setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent event) {
-				// TODO Auto-generated method stub
-				primaryStage.setScene(scene);
-				soundManager.playMainSong();
-				boardManager1.generateNewTile();
-				boardManager1.generateNewTile();
-				boardManager2.generateNewTile();
-				boardManager2.generateNewTile();
-			}
-		});
-		startPane.getQuitButton().setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				// TODO Auto-generated method stub
-				Platform.exit();
-				System.exit(0);
-			}
-		});
-		retryPane.getGameStartButton().setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				// TODO Auto-generated method stub
-				primaryStage.setScene(startScene);
-				boardManager1.resetBoard();
-				boardManager2.resetBoard();
-				dataPane.getChildren().add(soundManager.getSoundButton());
-				scorePane.reset();
-				ArrowButtonEventHandler.isDead = false;
-				soundManager.stopGameOverSong();
-				soundManager.getSoundButton().setSong("mainSong");
-			}
-		});
-		retryPane.getQuitButton().setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				// TODO Auto-generated method stub
-				Platform.exit();
-				System.exit(0);
-			}
-		});
-		
-
-		scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
-			if (key.getCode() == KeyCode.DOWN) {
-				controlPane.getDownButton().fire();
-			}
-			if (key.getCode() == KeyCode.UP) {
-				controlPane.getUpButton().fire();
-			}
-			if (key.getCode() == KeyCode.LEFT) {
-				controlPane.getLeftButton().fire();
-			}
-			if (key.getCode() == KeyCode.RIGHT) {
-				controlPane.getRightButton().fire();
-			}
-			if (logic.ArrowButtonEventHandler.isDead) {
-				retryPane.setYourScore(scorePane.getScore());
-				primaryStage.setScene(retryScene);
-				soundManager.stopMainSong();
-				retryPane.getChildren().add(soundManager.getSoundButton());
-				if (soundManager.getSoundButton().isSoundOn()) {
-					soundManager.playGameOverSong();
-				}
-				soundManager.getSoundButton().setSong("gameOverSong");
-				
-			}
-		});
-
+		eventManager.setupStartPaneStartButton(scene);
+		eventManager.setupStartPaneQuitButton();
+		eventManager.setupRetryPaneStartButton(startScene, dataPane, scorePane);
+		eventManager.setupRetryPaneQuitButton();
+		eventManager.setupSceneKeyHandler(scene, scorePane, retryScene);
 
 	}
+
 	public void createGameMaterial() {
 		scorePane = new ScorePane();
 		controlPane = new ControlPane();
@@ -192,21 +126,6 @@ public class Main extends Application {
 		startPane = new StartPane();
 		retryPane = new RetryPane();
 		soundManager = new SoundManager(new SoundButton());
-	}
-
-	public ControlPane getControlPane() {
-		return controlPane;
-	}
-
-	public ScorePane getScorePane() {
-		return scorePane;
-	}
-
-	public BoardPane getBoardPane1() {
-		return boardPane1;
-	}
-	public BoardPane getBoardPane2() {
-		return boardPane2;
 	}
 
 	public static void main(String[] args) {
